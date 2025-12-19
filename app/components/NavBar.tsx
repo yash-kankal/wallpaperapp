e0ca72c94c748f362779bcc963f97a0f96b745c2
Yash Kankal <yash.kankal@pangian.com>
2025-12-18 17:02:59 -0700

    Add project code

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className='w-screen bg-[#232b2b] h-18 flex items-center justify-center fixed top-0 z-40'>
+
+        <img src="/logo.png" alt="no image" width={200}  />
+
+        
+    </nav>
+  )
+}
+
+export default NavBar
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+        <NavBar ></NavBar>
+
+        <div className="mt-15">
+
         {children}
+        </div>
+        
       </body>
     </html>
   );
