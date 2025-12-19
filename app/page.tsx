135566f4325843b1e4ef2248ced5812ebe7298f1
Yash Kankal <yash.kankal@pangian.com>
2025-12-18 17:02:59 -0700

    Add project code

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,97 @@
-import Image from "next/image";
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  type photoDataType = {
+    id: string;
+    author: string;
+    download_url: string;
+  };
+
+  const [photoData, setPhotoData] = useState<photoDataType[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("https://picsum.photos/v2/list?page=1&limit=200");
+      setPhotoData(response.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex min-h-screen w-screen items-center justify-center bg-[#0e1111] text-white">
+        <p className="text-6xl">Loading...</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <div className="flex min-h-screen items-center justify-center  font-sans bg-[#0e1111]">
+      <div className="min-h-screen p-8 grid grid-cols-4 gap-8">
+        {photoData.map((pd) => (
+          <div
+            key={pd.id}
+            className="
+              group relative overflow-hidden rounded-2xl
+              bg-white dark:bg-zinc-900
+              shadow-md hover:shadow-[0_20px_50px_rgba(0,0,0,0.35)]
+              dark:hover:shadow-[0_20px_50px_rgba(255,255,255,0.12)]
+              transition-shadow duration-300
+            "
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+            <img
+              src={`https://picsum.photos/id/${pd.id}/350/500`}
+              alt={pd.author}
+              className="
+                w-full h-full object-cover
+                transition-transform duration-300
+                group-hover:scale-105
+              "
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+            <p
+              className="
+                absolute bottom-5 left-4
+                text-white font-bold text-sm
+                opacity-0 transition-opacity duration-300
+                group-hover:opacity-100
+              "
+            >
+              {pd.author}
+            </p>
+           <a
+  href={pd.download_url}
+  download
+  target="_blank"
+  rel="noopener noreferrer"
+  className="
+    absolute bottom-4 right-4
+    bg-green-600 text-white text-sm font-semibold
+    rounded px-3 py-1
+    opacity-0 transition-opacity duration-300
+    group-hover:opacity-100
+  "
+>
+  Download
+</a>
+
+
+            
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
